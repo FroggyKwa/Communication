925cfb90462e958a1a78ab2fe5f3bc92c68b7739
ReptileSD <50035939+ReptileSD@users.noreply.github.com>
2022-02-28 22:43:37 +1000

    Update app B.py

--- client/app B.py
+++ client/app B.py
@@ -5,21 +5,31 @@ api_url = 'https://cowork.asap-it.tech'
 
 status = 0
 
+color = ['green', 'blue', 'red', 'yellow']
+
 
 
 window = Tk()
 window.title("Application B")
 window.geometry("400x600")
 
+l1 = Label(text=status,
+           font="Arial 32")
+
 def task():
     r = requests.get(f'{api_url}/getState/')
-    global status
-    #status = r
+    global status, l1
+    status = r.json()
+
+    l1.config(bg=color[status - 1])
+    l1['text'] = status + 1
 
     print(r.json())
-    window.after(2000, task)  # reschedule event in 2 seconds
+    window.after(200, task)  # reschedule event in 2 seconds
 
-window.after(2000, task)
+l1.pack()
+
+window.after(200, task)
 
 
 window.mainloop()
